22bc56423687c619b71728bcd1b450ad94281813
Ramy-Badr-Ahmed <ramy.badr@hotmail.com>
2024-09-11 19:44:08 +0200

    Actions Test on Stack.hs

--- src/DataStructures/Stack.hs
+++ src/DataStructures/Stack.hs
@@ -58,6 +58,6 @@ main :: IO ()
 main = do
     let input = [1, 2, 3, 4, 5]
     let (result, emptyBefore, emptyAfter) = testStack input
-    print result          -- Expected output: [Just 5, Just 4, Just 3, Just 2, Just 1]
-    print emptyBefore     -- Expected output: False
-    print emptyAfter      -- Expected output: True
+    print result           -- Expected output: [Just 5, Just 4, Just 3, Just 2, Just 1]
+    print emptyBefore      -- Expected output: False
+    print emptyAfter       -- Expected output: True
